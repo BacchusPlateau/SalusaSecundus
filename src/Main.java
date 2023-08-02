c80e4e1cb35e1b7556f9518ba2338a836d42acc0
BacchusPlateau <straightener@hotmail.com>
2023-08-01 18:30:43 -0700

    First checkin

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You wake up from what seems like a year's long sleep.   You are in a room with walls, ceilings and floor of stone.");
+        System.out.println("Command?");
+
+        String command = scanner.nextLine();
+        System.out.println("Your command was: " + command);
+
+
+
+    }
+}
