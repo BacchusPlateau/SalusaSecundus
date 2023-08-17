875c0d75865e7a5670ada43ff1de5ffe03aa74dc
bacchusplateau <straightener@hotmail.com>
2023-08-17 07:00:43 -0700

    zz

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Pyramid pyramid = new Pyramid();
-        pyramid.GameLoop();
+       // Pyramid pyramid = new Pyramid();
+       // pyramid.GameLoop();
 
-
+        Stage1 s = new Stage1();
+        s.Stage1();
 
     }
 }
--- src/Stage1.java
+++ src/Stage1.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Stage1 {
     public void Stage1() {
+
+
+        // Initialize the game
+        int score = 0;
+        String command = "";
+        int room = 1;
+        boolean hasAnkh = false;
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("***********************************************************************************");
         System.out.println("*                                                                                 *");
@@ -12,8 +23,13 @@ public class Stage1 {
         System.out.println("*                                                                                 *");
         System.out.println("***********************************************************************************");
         System.out.println("");
-        System.out.println("You wake up from what seems like a year's long sleep.   You are in a room with walls, ceilings and floor of stone.");
+        System.out.println("You are in a room with walls, ceilings and floor of stone.");
         System.out.println("Command?");
+        // Get player command
+        System.out.print("> ");
+        command = sc.nextLine();
+
+        System.out.println("Your command was " + command);
 
     }
 }
